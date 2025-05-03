da59b051a2231d972975c9a4f1f1688b425f2bf4
Yurii_Moroziuk <dniprik.online@gmail.com>
2025-05-03 23:08:55 +0300

    Add Validation and @Data annotation

--- songservice/src/main/java/song/dto/SongDto.java
+++ songservice/src/main/java/song/dto/SongDto.java
@@ -3,10 +3,14 @@ package song.dto;
 import com.drew.lang.annotations.NotNull;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
+import lombok.Data;
 
+@Data
 public class SongDto {
     @NotNull
+    @Positive
     private Long id;
 
     @NotBlank
@@ -26,52 +30,4 @@ public class SongDto {
 
     @Pattern(regexp = "^(19|20)\\d{2}$", message = "Year must be between 1900 and 2099")
     private String year;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getArtist() {
-        return artist;
-    }
-
-    public void setArtist(String artist) {
-        this.artist = artist;
-    }
-
-    public String getAlbum() {
-        return album;
-    }
-
-    public void setAlbum(String album) {
-        this.album = album;
-    }
-
-    public String getDuration() {
-        return duration;
-    }
-
-    public void setDuration(String duration) {
-        this.duration = duration;
-    }
-
-    public String getYear() {
-        return year;
-    }
-
-    public void setYear(String year) {
-        this.year = year;
-    }
 }
